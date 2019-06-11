6145708434896fd8995d6262a36367eccf57fc7d
serhatyavuzyigit <soyavuzyigit@gmail.com>
2019-06-11 09:13:49 +0300

    Some design changes

--- app/src/main/java/com/etu/recipemanagement/DetailActivity.java
+++ app/src/main/java/com/etu/recipemanagement/DetailActivity.java
@@ -2,22 +2,23 @@ package com.etu.recipemanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Scroller;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private EditText textDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        textDetails = findViewById(R.id.textDetails);
-        textDetails.setMinLines(1);
-        textDetails.setMaxLines(Integer.MAX_VALUE);
 
 
+    }
+
+    public void addItemToList(View view) {
 
     }
 }
