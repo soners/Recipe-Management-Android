652c62bcfcce93fd0f89e15d95c00454c6da0f44
serhatyavuzyigit <soyavuzyigit@gmail.com>
2019-06-11 21:53:09 +0300

    ListView added for ingredients

--- app/src/main/java/com/etu/recipemanagement/DetailActivity.java
+++ app/src/main/java/com/etu/recipemanagement/DetailActivity.java
@@ -4,15 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Scroller;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
-
     private EditText textDescription;
+    private EditText ingredientName;
     private ListView ingredientsListView;
+    private List<String> ingredientsNameList;
+    private ArrayAdapter ingredientsAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +28,23 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         textDescription = findViewById(R.id.editTextDescription);
-        ingredientsListView = findViewById(R.id.ingredientsList);
+        ingredientName = findViewById(R.id.ingredientNameInput);
+        ingredientsListView = findViewById(R.id.ingredintsListView);
+        ingredientsNameList = new ArrayList<>();
 
     }
-
 
     public void addItemToIngredientList(View view) {
-        Intent toIngredient = new Intent(this, IngredientActivity.class);
+        if(ingredientsNameList.get(0) == null) {
+            // first time
+            ingredientsNameList.add(ingredientName.getText().toString());
+            ingredientsAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ingredientsNameList);
+            ingredientsListView.setAdapter(ingredientsAdapter);
+        } else {
+            ingredientsNameList.add(ingredientName.getText().toString());
+            ingredientsAdapter.notifyDataSetChanged();
+        }
     }
+
+
 }
--- app/src/main/java/com/etu/recipemanagement/MainActivity.java
+++ app/src/main/java/com/etu/recipemanagement/MainActivity.java
@@ -1,5 +1,6 @@
 package com.etu.recipemanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_main);
+
+        Intent toDetail = new Intent(this,DetailActivity.class);
+        startActivity(toDetail);
     }
 }
