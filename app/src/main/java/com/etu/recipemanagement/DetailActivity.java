10dfefd35c82876f4c374caf4ab56f0fc8a16279
serhatyavuzyigit <soyavuzyigit@gmail.com>
2019-06-11 05:54:33 +0300

    Text detail design and ingredient adding buttong added.

--- app/src/main/java/com/etu/recipemanagement/DetailActivity.java
+++ app/src/main/java/com/etu/recipemanagement/DetailActivity.java
@@ -2,12 +2,22 @@ package com.etu.recipemanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private EditText textDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        textDetails = findViewById(R.id.textDetails);
+        textDetails.setMinLines(1);
+        textDetails.setMaxLines(Integer.MAX_VALUE);
+
+
+
     }
 }
