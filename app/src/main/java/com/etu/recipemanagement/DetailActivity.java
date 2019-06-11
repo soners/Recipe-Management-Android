e57598003e02ef1d68c11c006e4793b03537ee90
serhatyavuzyigit <soyavuzyigit@gmail.com>
2019-06-11 12:24:15 +0300

    Ingredient Activity and it's xml added.

--- app/src/main/java/com/etu/recipemanagement/DetailActivity.java
+++ app/src/main/java/com/etu/recipemanagement/DetailActivity.java
@@ -1,24 +1,31 @@
 package com.etu.recipemanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Scroller;
 
 public class DetailActivity extends AppCompatActivity {
 
+
+    private EditText textDescription;
+    private ListView ingredientsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-
+        textDescription = findViewById(R.id.editTextDescription);
+        ingredientsListView = findViewById(R.id.ingredientsList);
 
     }
 
-    public void addItemToList(View view) {
 
+    public void addItemToIngredientList(View view) {
+        Intent toIngredient = new Intent(this, IngredientActivity.class);
     }
 }
