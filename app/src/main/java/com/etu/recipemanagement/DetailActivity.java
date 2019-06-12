edad6283a5ba04d563c9a2887f3900b0f982a1a3
serhatyavuzyigit <soyavuzyigit@gmail.com>
2019-06-12 16:59:25 +0300

    ingredients lisview added

--- app/src/main/java/com/etu/recipemanagement/DetailActivity.java
+++ app/src/main/java/com/etu/recipemanagement/DetailActivity.java
@@ -1,13 +1,14 @@
 package com.etu.recipemanagement;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.Scroller;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,36 +16,44 @@ import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private EditText textDescription;
+    private LinearLayout ingredients;
+    private LinearLayout details;
+    private ArrayList<String> ingredientsNames;
     private EditText ingredientName;
+    private IngredientAdapter ingredientAdapter;
     private ListView ingredientsListView;
-    private List<String> ingredientsNameList;
-    private ArrayAdapter ingredientsAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        textDescription = findViewById(R.id.editTextDescription);
         ingredientName = findViewById(R.id.ingredientNameInput);
-        ingredientsListView = findViewById(R.id.ingredintsListView);
-        ingredientsNameList = new ArrayList<>();
-
+        ingredientsNames = new ArrayList<>();
+        ingredientAdapter = new IngredientAdapter(this, ingredientsNames);
+        ingredientsListView = findViewById(R.id.ingredientsListView);
+        init();
     }
 
-    public void addItemToIngredientList(View view) {
-        if(ingredientsNameList.get(0) == null) {
-            // first time
-            ingredientsNameList.add(ingredientName.getText().toString());
-            ingredientsAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ingredientsNameList);
-            ingredientsListView.setAdapter(ingredientsAdapter);
-        } else {
-            ingredientsNameList.add(ingredientName.getText().toString());
-            ingredientsAdapter.notifyDataSetChanged();
-        }
+    private void init() {
+        details = findViewById(R.id.details);
+        ingredients = findViewById(R.id.ingredient);
+
+        Button button = findViewById(R.id.add_ingredient);
+        button.setOnClickListener((v) -> {
+            ingredients.setVisibility(View.VISIBLE);
+            details.setVisibility(View.GONE);
+        });
+
+        Button back = findViewById(R.id.ing_back);
+        back.setOnClickListener((v) -> {
+            ingredients.setVisibility(View.GONE);
+            details.setVisibility(View.VISIBLE);
+        });
     }
 
-
+    public void addIngredientsToList(View view) {
+        ingredientsNames.add(ingredientName.getText().toString());
+        ingredientAdapter = new IngredientAdapter(this,ingredientsNames);
+        ingredientsListView.setAdapter(ingredientAdapter);
+    }
 }
