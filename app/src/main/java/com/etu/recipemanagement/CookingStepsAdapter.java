b664a85ab3c1f7025f916af58901c87c668ab55c
serhatyavuzyigit <soyavuzyigit@gmail.com>
2019-06-12 20:01:20 +0300

    cooking steps added

--- app/src/main/java/com/etu/recipemanagement/CookingStepsAdapter.java
+++ app/src/main/java/com/etu/recipemanagement/CookingStepsAdapter.java
@@ -1,4 +1,61 @@
 package com.etu.recipemanagement;
 
-public class CookingStepsAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CookingStepsAdapter extends BaseAdapter {
+
+
+    private Context context;
+    private ArrayList<String> cookingSteps;
+
+    public CookingStepsAdapter(Context context, ArrayList<String> cookingSteps) {
+        this.context = context;
+        this.cookingSteps = cookingSteps;
+    }
+
+    @Override
+    public int getCount() {
+        return cookingSteps.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return cookingSteps.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // inflate the layout for each list row
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).
+                    inflate(R.layout.ingredient_adapter, parent, false);
+        }
+
+        // get current item to be displayed
+        String currentItem = (String) getItem(position);
+
+        // get the TextView for item name and item description
+        // get the TextView for item name and item description
+        TextView textViewItemName = (TextView)
+                convertView.findViewById(R.id.name);
+
+
+        //sets the text for item name and item description from the current item object
+        textViewItemName.setText(currentItem);
+
+        // returns the view for the current row
+        return convertView;
+    }
 }
